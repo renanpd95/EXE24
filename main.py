f1e7e538a294ef14639b9a41c62d22207a398552
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-12 18:10:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+name = input("Nome do aluno: ")
+cod = int(input("Prontuario do Aluno: "))
+tel = int(input("Telefone: "))
+email = input("E-mail: ")
+
+os.system('clear')
+
+print("\nNome: ",name,"\nProntuario: ",cod,"\nTelefone: ",tel,"\nE-mail: ",email)
